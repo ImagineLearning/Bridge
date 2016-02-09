e0cb8a41b366397cfb51c7cb47aa84e04a8e06c5
Jon Bell <jon.bell@imaginelearning.com>
2016-02-09 13:27:23 -0700

    Updating common assembly info

--- .build/CommonAssemblyInfo.cs
+++ .build/CommonAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.11")]
-[assembly: AssemblyFileVersion("1.11.0")]
-[assembly: AssemblyCompany("Object.NET, Inc.")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCompany("Object.NET, Inc., Imagine Learning, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2016 Object.NET, Inc.")]
