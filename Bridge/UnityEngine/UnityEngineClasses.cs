16cc48d7237536afa737a5c6057fc7385c837630
Jon Bell <jon.bell@imaginelearning.com>
2016-02-09 13:27:02 -0700

    Renaming Bridge nuget package to Bridge.Unity and including unity engine stubs. Deleting html5 project as it is not relevant to unity

---
+++ Bridge/UnityEngine/UnityEngineClasses.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Bridge;
+
+namespace UnityEngine
+{
+	[External]
+	public class Transform
+	{
+		
+	}
+
+	[External]
+	public class GameObject
+	{
+		
+	}
+
+	[External]
+	public class Object
+	{
+		
+	}
+
+	[External]
+	public class AccelerationEvent
+	{
+		
+	}
+
+	[External]
+	public class Texture2D
+	{
+		
+	}
+
+	[External]
+	public class AudioClip
+	{
+		
+	}
+
+	[External]
+	public class Camera
+	{
+		public static Camera main { get; private set; }
+
+		public Vector2 ViewportToWorldPoint(Vector2 viewportPoint)
+		{
+			return default(Vector2);
+		}
+		public Vector2 ViewportToWorldPoint(Vector3 viewportPoint)
+		{
+			return default(Vector2);
+		}
+	}
+
+	[External]
+	public static class Mathf
+	{
+		public static float Pow(float num, int pow)
+		{
+			return 0;
+		}
+
+		public static float Abs(float num)
+		{
+			return 0;
+		}
+
+		public static float Min(float num1, float num2)
+		{
+			return 0;
+		}
+
+		public static float Max(float num1, float num2)
+		{
+			return 0;
+		}
+
+		public static float PI { get; private set; }
+
+		public static float Sin(float num)
+		{
+			return 0;
+		}
+
+		public static float Cos(float num)
+		{
+			return 0;
+		}
+
+		public static float Sqrt(float num1)
+		{
+			return 0;
+		}
+	}
+
+	[External]
+	public static class Resources
+	{
+		public static Object[] FindObjectsOfTypeAll(Type type)
+		{
+			return null;
+		}
+	}
+
+}
---
+++ Bridge/UnityEngine/UnityEngineStructs.cs
@@ -0,0 +1,156 @@
+﻿using Bridge;
+
+namespace UnityEngine
+{
+	[External]
+	public struct Vector2
+	{
+		public float x;
+		public float y;
+
+
+		public Vector2(float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public static Vector2 operator +(Vector2 c1, Vector2 c2)
+		{
+			return default(Vector2);
+		}
+		public static Vector2 operator *(Vector2 c1, float f)
+		{
+			return default(Vector2);
+		}
+		public static Vector2 operator *(float f, Vector2 c1)
+		{
+			return default(Vector2);
+		}
+	}
+
+	[External]
+	public struct Vector3
+	{
+		public float x;
+		public float y;
+		public float z;
+
+		public Vector3(float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = 0;
+		}
+
+		public Vector3(float x, float y, float z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+		public static Vector3 operator +(Vector3 c1, Vector3 c2)
+		{
+			return default(Vector3);
+		}
+
+		public static Vector3 operator *(Vector3 c1, float f)
+		{
+			return default(Vector3);
+		}
+
+		public static Vector3 operator *(float f, Vector3 c1)
+		{
+			return default(Vector3);
+		}
+
+		public static float Distance(Vector3 v1, Vector3 v2)
+		{
+			return 0;
+		}
+	}
+
+	[External]
+	public struct Rect
+	{
+		public float x;
+		public float y;
+		public float width;
+		public float height;
+
+		public Rect(float x, float y, float width, float height)
+		{
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+		}
+
+	}
+	
+	[External]
+	public struct Ray
+	{
+		
+	}
+
+	[External]
+	public enum KeyCode
+	{
+		
+	}
+	[External]
+	public struct Touch
+	{
+		
+	}
+	[External]
+	public enum TouchPhase
+	{
+		Began,
+		Canceled,
+		Ended
+	}
+
+	[External]
+	public struct Color
+	{
+		
+	}
+
+	[External]
+	public struct Bounds
+	{
+		
+	}
+
+	[External]
+	public enum TextureWrapMode
+	{
+		
+	}
+
+	[External]
+	public enum FilterMode
+	{
+		
+	}
+
+	[External]
+	public enum TextureFormat
+	{
+		
+	}
+
+	[External]
+	public struct Color32
+	{
+		
+	}
+
+	[External]
+	public struct Quaternion
+	{
+		
+	}
+}
