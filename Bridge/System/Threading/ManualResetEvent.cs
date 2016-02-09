a1a6a0cb53ed7e6b70b5531b39b5856b6e60a61e
Jon Bell <jon.bell@imaginelearning.com>
2016-02-09 16:29:32 -0700

    Moving manual reset event to the right namespace and corresponding location.

---
+++ Bridge/System/Threading/ManualResetEvent.cs
@@ -0,0 +1,23 @@
+﻿using Bridge;
+
+namespace System.Threading
+{
+	[External]
+	public class ManualResetEvent
+	{
+		public ManualResetEvent(bool initialState)
+		{
+
+		}
+
+		public void Set()
+		{
+
+		}
+
+		public bool WaitOne(int millisecondsTimeout = 0)
+		{
+			return false;
+		}
+	}
+}
