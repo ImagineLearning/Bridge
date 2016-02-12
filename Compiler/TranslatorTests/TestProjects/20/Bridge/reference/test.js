ed8249462a029dd2caf0e7005c1966dac11ae395
Jon Bell <jon.bell@imaginelearning.com>
2016-02-12 14:43:41 -0700

    Test now running and failing as expected.

--- Compiler/TranslatorTests/TestProjects/20/Bridge/reference/test.js
+++ Compiler/TranslatorTests/TestProjects/20/Bridge/reference/test.js
@@ -5,16 +5,16 @@
 
     Bridge.define('Test.GenericsTest', {
         genericListTest: function () {
-            return new System.Collections.Generic.List$1(String)();
+            return new (System.Collections.Generic.List(String))();
         },
         genericDictionaryTest: function () {
-            return new System.Collections.Generic.Dictionary$2(String,System.Int32)();
+            return new (System.Collections.Generic.Dictionary(String,System.Int32))();
         },
         genericListTest2: function () {
-            return new System.Collections.Generic.List$1(Test.TestClass1)();
+            return new (System.Collections.Generic.List(Test.TestClass1))();
         },
         genericDictionaryTest2: function () {
-            return new System.Collections.Generic.Dictionary$2(Test.TestClass1,Test.TestClass2)();
+            return new (System.Collections.Generic.Dictionary(Test.TestClass1,Test.TestClass2))();
         }
     });
     
