3dd6eaafac3578a8b94fa21bfe07abd9c447821e
Jon Bell <jon.bell@imaginelearning.com>
2016-02-12 14:29:23 -0700

    scaffolding out test20 - generics test

---
+++ Compiler/TranslatorTests/TestProjects/20/Bridge/reference/test.js
@@ -0,0 +1,26 @@
+﻿/* global Bridge */
+
+(function (globals) {
+    "use strict";
+
+    Bridge.define('Test.GenericsTest', {
+        genericListTest: function () {
+            return new System.Collections.Generic.List$1(String)();
+        },
+        genericDictionaryTest: function () {
+            return new System.Collections.Generic.Dictionary$2(String,System.Int32)();
+        },
+        genericListTest2: function () {
+            return new System.Collections.Generic.List$1(Test.TestClass1)();
+        },
+        genericDictionaryTest2: function () {
+            return new System.Collections.Generic.Dictionary$2(Test.TestClass1,Test.TestClass2)();
+        }
+    });
+    
+    Bridge.define('Test.TestClass1');
+    
+    Bridge.define('Test.TestClass2');
+    
+    Bridge.init();
+})(this);
