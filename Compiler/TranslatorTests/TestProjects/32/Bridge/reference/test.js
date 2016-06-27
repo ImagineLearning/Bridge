65c12e6792c62da0e02a1c3966ac17bcbfc752f5
Adam Christiansen <adam.j.christiansen@gmail.com>
2016-06-27 15:45:18 -0600

    Changing Bridge.Nullable.eq(var 1, var2) to var1 === var2

---
+++ Compiler/TranslatorTests/TestProjects/32/Bridge/reference/test.js
@@ -0,0 +1,25 @@
+﻿/* global Bridge */
+
+(function (globals) {
+    "use strict";
+
+    Bridge.define('Test.ClassWithNullable', {
+        _myNullableInt: null,
+        MyNullableBool: null,
+        Test1: function () {
+            if (this.MyNullableBool === true) {
+                this._myNullableInt = 100;
+            }
+    
+            if (this._myNullableInt > 99) {
+                this.MyNullableBool = false;
+            }
+    
+            if (this._myNullableInt === 100) {
+                this._myNullableInt = 101;
+            }
+        }
+    });//end of class Test.ClassWithNullable
+    
+    Bridge.init();
+})(this);
